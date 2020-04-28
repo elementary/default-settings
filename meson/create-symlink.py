e36bbf34f77b04adcbdab880f13831b33e221fc4
Daniel Foré <daniel@elementary.io>
2020-04-27 17:19:32 -0700

    Use symlink script from systemd

--- meson/create-symlink.py
+++ meson/create-symlink.py
@@ -1,25 +1,12 @@
-#!/usr/bin/env python3
+#!/bin/sh
+set -eu
 
-import argparse
-import os
+# this is needed mostly because $DESTDIR is provided as a variable,
+# and we need to create the target directory...
 
-parser = argparse.ArgumentParser(description='Create a symlink')
-parser.add_argument('--src', nargs=1)
-parser.add_argument('--dest', nargs=1)
-args = parser.parse_args()
-
-inst_prefix = os.environ['MESON_INSTALL_PREFIX']
-dest_prefix = os.environ['MESON_INSTALL_DESTDIR_PREFIX']
-
-src = os.path.basename(os.path.join(inst_prefix, args.src[0]))
-dest = os.path.join(dest_prefix, args.dest[0])
-
-if os.path.exists(dest):
-    if os.path.isdir(dest):
-        print('Folder "' + dest + '" already exists, no symlink will be created')
-    else:
-        print('File "' + dest + '" already exists, no symlink will be created')
-else:
-    print('Linking ' + dest + ' to ' + src)
-    os.symlink(src, dest)
-
+mkdir -vp "$(dirname "${DESTDIR:-}$2")"
+if [ "$(dirname $1)" = . -o "$(dirname $1)" = .. ]; then
+    ln -vfs -T -- "$1" "${DESTDIR:-}$2"
+else
+    ln -vfs -T --relative -- "${DESTDIR:-}$1" "${DESTDIR:-}$2"
+fi
