ed6d698a65f6f1204cafec741102692188a68e8c
Dominic Hayes - Feren OS Dev <ferenosdev@outlook.com>
2024-03-29 19:19:57 +0000

    Update qt-qpa-platformtheme.sh

--- profile.d/qt-qpa-platformtheme.sh
+++ profile.d/qt-qpa-platformtheme.sh
@@ -1,3 +1,3 @@
-if [ -z "$QT_QPA_PLATFORMTHEME" ] && [ "$XDG_CURRENT_DESKTOP" == "Pantheon" ] then
+if [ -z "$QT_QPA_PLATFORMTHEME" ] && [ "$XDG_CURRENT_DESKTOP" == "Pantheon" ]; then
   export QT_QPA_PLATFORMTHEME=gtk3
 fi
