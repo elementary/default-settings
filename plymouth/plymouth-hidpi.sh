03cf5da146ece7e31c5f7b28a1eb748d88ecc31f
rreina <ruben.reina.dev@gmail.com>
2025-03-29 23:02:36 +0100

    Fix service target and ensure Gala is available for xrdb command to work

--- plymouth/plymouth-hidpi.sh
+++ plymouth/plymouth-hidpi.sh
@@ -1,50 +1,45 @@
 #!/bin/bash
 
 CONFIG_FILE="/etc/plymouth/plymouthd.conf"
-DAEMON_SECTION="[Daemon]"
-SCALE_SETTING="DeviceScale=1"
 HIDPI_THRESHOLD=180 # Min DPI of a HiDPI screen
 
+TIMEOUT=300  # Set the maximum wait time in seconds
+ELAPSED=0
+
+# Wait until Gala is running or timeout occurs
+while ! pgrep -x "gala" > /dev/null && [ $ELAPSED -lt $TIMEOUT ]; do
+    sleep 1
+    ((ELAPSED++))
+done
+
+if [ $ELAPSED -ge $TIMEOUT ]; then
+    echo "Timeout reached while waiting for Gala to start" >&2
+    exit 1
+fi
+
 # Get current DPI
+export DISPLAY=":0"
 DPI=$(xrdb -query | grep dpi | awk '{print $2}')
 
-# Adds
-# [Daemon]
-# DeviceScale=1
-# only if missing in the file
-add_setting() {
-    # Remove first line if it's blank
-    sed -i '1{/^$/d}' "$CONFIG_FILE"
-
-    if ! grep -q "$DAEMON_SECTION" "$CONFIG_FILE"; then
-        echo -e "$DAEMON_SECTION" >> "$CONFIG_FILE"
-    fi
-
-    sed -i "/$DAEMON_SECTION/a $SCALE_SETTING" "$CONFIG_FILE"
-}
-
-# Creates the configuration file with the necessary settings.
 create_config() {
-    echo -e "$DAEMON_SECTION\n$SCALE_SETTING" > "$CONFIG_FILE"
+    cat <<EOF > "$CONFIG_FILE"
+[Daemon]
+DeviceScale=1
+EOF
+    logger -t plymouth-hidpi "Created new config: '$CONFIG_FILE' with HiDPI setting"
 }
 
 apply_hidpi_setting() {
     if [[ "$DPI" -ge "$HIDPI_THRESHOLD" ]]; then
-        if [[ -f "$CONFIG_FILE" ]]; then
-            if ! grep -q "^DeviceScale" "$CONFIG_FILE"; then
-                add_setting
-                logger -t plymouth-hidpi "Added '$SCALE_SETTING' to '$CONFIG_FILE'"
-            else
-                logger -t plymouth-hidpi "HiDPI setting already present in '$CONFIG_FILE' no changes made"
-            fi
-        else
+        if [[ ! -f "$CONFIG_FILE" ]]; then
             create_config
-            logger -t plymouth-hidpi "Created new config: '$CONFIG_FILE' with HiDPI setting"
-        fi
 
-        # Apply the changes
-        update-initramfs -u
-        logger -t plymouth-hidpi "Updated initramfs after HiDPI change"
+            # Apply the changes
+            update-initramfs -u
+            logger -t plymouth-hidpi "Updated initramfs after HiDPI change"
+        else
+            logger -t plymouth-hidpi "HiDPI config file already exists, no changes made"
+        fi
     else
         logger -t plymouth-hidpi "Skipped HiDPI settings (DPI: $DPI, Threshold: $HIDPI_THRESHOLD)"
     fi
