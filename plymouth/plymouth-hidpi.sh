17167810810bef1003237eb607f02b4f8335cbf6
rreina <ruben.reina.dev@gmail.com>
2025-03-30 20:48:26 +0200

    Support Wayland & X11 by reading the scaling factor from Gnome's monitors.xml file. Also respect get_option('bindir') for improved configuration handling.

--- plymouth/plymouth-hidpi.sh
+++ plymouth/plymouth-hidpi.sh
@@ -1,25 +1,43 @@
 #!/bin/bash
 
 CONFIG_FILE="/etc/plymouth/plymouthd.conf"
-HIDPI_THRESHOLD=180 # Min DPI of a HiDPI screen
 
-TIMEOUT=300  # Set the maximum wait time in seconds
+TIMEOUT_SECONDS=300 # Maximum wait time in seconds
 ELAPSED=0
 
 # Wait until Gala is running or timeout occurs
-while ! pgrep -x "gala" > /dev/null && [ $ELAPSED -lt $TIMEOUT ]; do
-    sleep 1
-    ((ELAPSED++))
+while ! pgrep -x "gala" > /dev/null && [ $ELAPSED -lt $TIMEOUT_SECONDS ]; do
+    sleep 5
+    ((ELAPSED += 5))
 done
 
-if [ $ELAPSED -ge $TIMEOUT ]; then
+if [ $ELAPSED -ge $TIMEOUT_SECONDS ]; then
     echo "Timeout reached while waiting for Gala to start" >&2
     exit 1
 fi
 
-# Get current DPI
-export DISPLAY=":0"
-DPI=$(xrdb -query | grep dpi | awk '{print $2}')
+# Get the active user based on the currently active graphical session
+ACTIVE_USER=$(who | grep -E '(:[0-9]+)' | awk '{print $1}' | head -n 1)
+
+if [[ -z "$ACTIVE_USER" ]]; then
+    logger -t plymouth-hidpi "No active user with a graphical session."
+    exit 1
+fi
+
+USER_HOME=$(eval echo ~$ACTIVE_USER)  # Get the home directory of the active user
+MONITORS_XML="$USER_HOME/.config/monitors.xml"
+
+if [[ ! -f "$MONITORS_XML" ]]; then
+    logger -t plymouth-hidpi "Monitors XML file not found for user: $PRIMARY_USER"
+    exit 1
+fi
+
+PRIMARY_MONITOR_SCALE=$(awk '
+    /<primary>yes<\/primary>/ {print prev; exit}
+    {prev=$0}
+' $MONITORS_XML | grep -oP '<scale>\K[0-9]+')
+# Scale=2 means 2x resolution ('Retina Display' in Apple computers)
+logger -t plymouth-hidpi "Primary monitor scale: '$PRIMARY_MONITOR_SCALE'" 
 
 create_config() {
     cat <<EOF > "$CONFIG_FILE"
@@ -30,7 +48,7 @@ EOF
 }
 
 apply_hidpi_setting() {
-    if [[ "$DPI" -ge "$HIDPI_THRESHOLD" ]]; then
+    if [[ "$PRIMARY_MONITOR_SCALE" -eq 2 ]]; then
         if [[ ! -f "$CONFIG_FILE" ]]; then
             create_config
 
